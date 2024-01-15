795dfd404e9433ad27c6ec5e076884105ab431fd
SantioMC <business@santio.me>
2024-01-14 23:56:14 -0500

    feat: Get doors functional

--- src/main/kotlin/me/santio/isekai/Isekai.kt
+++ src/main/kotlin/me/santio/isekai/Isekai.kt
@@ -1,10 +1,8 @@
 package me.santio.isekai
 
 import me.santio.isekai.commands.GamemodeCommand
-import me.santio.isekai.handler.block.DoorHandler
-import me.santio.isekai.helper.BlockRegistry
-import me.santio.isekai.helper.BlockRegistry.isKind
 import me.santio.isekai.helper.registerListener
+import me.santio.isekai.listeners.DoorListener
 import me.santio.isekai.listeners.GlobalBlockHandler
 import me.santio.isekai.listeners.PlayerListener
 import me.santio.isekai.worlds.IntroWorld
@@ -19,8 +17,7 @@ fun main() {
     val eventHandler = MinecraftServer.getGlobalEventHandler()
     eventHandler.registerListener(server, PlayerListener(server))
     eventHandler.registerListener(server, GlobalBlockHandler)
-
-    GlobalBlockHandler.register(DoorHandler) { it.isKind(BlockRegistry.BlockKind.DOOR) }
+    eventHandler.registerListener(server, DoorListener)
 
     server.start("0.0.0.0", 25565)
     println("Server started in ${System.currentTimeMillis() - started}ms")
